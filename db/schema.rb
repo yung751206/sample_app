e8873c01ed31a39a1b9ec44931d283540f0d3eac
yung751206@gmail.com <yung751206@gmail.com>
2016-08-07 15:28:10 -0700

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612222232) do
+ActiveRecord::Schema.define(version: 20160727054508) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160612222232) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
